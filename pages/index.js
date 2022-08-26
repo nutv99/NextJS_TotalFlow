605b6e0f369d5b15e4ae30ba9dd3fdf2db803492
nutv99 <nutv99@gmail.com>
2022-08-26 14:06:56 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -57,4 +57,27 @@ export default function Home() {
       </footer>
     </div>
   );
+} 
+
+function MainMenu() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About Us</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/blog/hello-world">
+          <a>Blog Post</a>
+        </Link>
+      </li>
+    </ul>
+  )
 }
+
